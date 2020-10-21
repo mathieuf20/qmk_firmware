b090350c87d6133bbdd35000e620717739dab5f7
Mathieu Fournier <contact@mathieufournier.ca>
2020-10-21 15:53:03 -0400

    Cleaning up and creation of definitions to straighten layout arrays

--- keyboards/lily58/keymaps/mathieuf20/keymap.c
+++ keyboards/lily58/keymaps/mathieuf20/keymap.c
@@ -34,13 +34,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                   `----------------------------'           '------''--------------------'
  * ***1: LCTL when held, ESC when tapped
  */
-
+ #define PARLEFT LSFT(KC_9)
+ #define PARRGHT LSFT(KC_0)
+ #define CTRLESC MT(MOD_LCTL,KC_ESC)
+ #define GOLOWER MO(_LOWER)
+ #define GORAISE MO(_RAISE)
  [_QWERTY] = LAYOUT(
-  KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
-  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS, \
-  MT(MOD_LCTL, KC_ESC), KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, LSFT(KC_9),  LSFT(KC_0),  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-                        KC_LALT, KC_LGUI, MO(_LOWER), KC_SPC, KC_ENT, MO(_RAISE), KC_BSPC, KC_RALT \
+  KC_GRV ,KC_1   ,KC_2   ,KC_3   ,KC_4   ,KC_5   ,                KC_6   ,KC_7   ,KC_8   ,KC_9   ,KC_0   ,KC_BSPC,\
+  KC_TAB ,KC_Q   ,KC_W   ,KC_E   ,KC_R   ,KC_T   ,                KC_Y   ,KC_U   ,KC_I   ,KC_O   ,KC_P   ,KC_MINS,\
+  CTRLESC,KC_A   ,KC_S   ,KC_D   ,KC_F   ,KC_G   ,                KC_H   ,KC_J   ,KC_K   ,KC_L   ,KC_SCLN,KC_QUOT,\
+  KC_LSFT,KC_Z   ,KC_X   ,KC_C   ,KC_V   ,KC_B   ,PARLEFT,PARRGHT,KC_N   ,KC_M   ,KC_COMM,KC_DOT ,KC_SLSH,KC_RSFT,\
+                          KC_LALT,KC_LGUI,GOLOWER,KC_SPC ,KC_ENT ,GORAISE,KC_BSPC,KC_RALT\
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -48,20 +52,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                    |  F7  |  F8  |  F9  | F10  | F11  | F12  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |   `  |   !  |   @  |   #  |   $  |   %  |-------.    ,-------|   ^  |   &  |   *  |   (  |   )  |   \  |
+ * |   /  |   !  |   @  |   #  |   $  |   %  |-------.    ,-------|   ^  |   &  |   *  |   (  |   )  |   \  |
  * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
- * | PREV | PLAY | NEXT | VOLUP|VOLDWN| MUTE |-------|    |-------|      |   _  |   +  |   ^  |   ç  |   à  |
+ * | PREV | PLAY | NEXT |VOLDWN|VOLUP | MUTE |-------|    |-------|   ~  |   _  |   +  |   ^  |   ç  |   à  |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |DELETE| RALT |
+ *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |DELETE| RGUI |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
+#define SBRKLFT RALT(KC_9)
+#define SBRKRGT RALT(KC_0)
+#define GORESET MO(_RESET)
+#define TILDE   ALGR(KC_RBRC)
+#define RSQWERT LM(_QWERTY, MOD_RSFT)
 [_LOWER] = LAYOUT( \
-  MO(_RESET),_______, _______, _______, _______, _______,                 _______, _______, _______,_______, _______, KC_DELETE,\
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
-  KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                    KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TILD, \
-  KC_MPRV, KC_MPLY, KC_MNXT, KC_VOLU, KC_VOLD, KC_MUTE, RALT(KC_9), RALT(KC_0), XXXXXXX, KC_UNDS, KC_PLUS, KC_LBRC, KC_RBRC, KC_BSLS, \
-                             _______, _______, _______, _______, KC_RSFT,  LM(_QWERTY, MOD_RSFT), KC_DELETE, _______\
+  GORESET,_______,_______,_______,_______,_______,                _______,_______,_______,_______,KC_PSCR,KC_DEL ,\
+  KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,KC_F6  ,                KC_F7  ,KC_F8  ,KC_F9  ,KC_F10 ,KC_F11 ,KC_F12 ,\
+  KC_GRV ,KC_EXLM,KC_AT  ,KC_HASH,KC_DLR ,KC_PERC,                KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_TILD,\
+  KC_MPRV,KC_MPLY,KC_MNXT,KC_VOLD,KC_VOLU,KC_MUTE,SBRKLFT,SBRKRGT,TILDE  ,KC_UNDS,KC_PLUS,KC_LBRC,KC_RBRC,KC_BSLS,\
+                          _______,_______,_______,_______,_______,RSQWERT,KC_DEL ,KC_RGUI\
 ),
 /* RAISE
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -69,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |   `  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  | HOME |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |-------.    ,-------| PGUP | Left | Down |  Up  |Right | END |
+ * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |-------.    ,-------| PGUP | Left | Down |  Up  | Right|  END |
  * |------+------+------+------+------+------|   {   |    |    }  |------+------+------+------+------+------|
  * |  F7  |  F8  |  F9  | F10  | F11  | F12  |-------|    |-------| PGDN |   -  |   =  |   ¨  |   Ç  |   À  |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
@@ -77,20 +86,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
-
+#define GREATER ALGR(KC_DOT)
+#define LSQWERT LM(_QWERTY, MOD_LSFT)
+#define BRKLEFT RALT(KC_7)
+#define BRKRGHT RALT(KC_8)
+#define SHFPSCR S(KC_PSCR)
 [_RAISE] = LAYOUT( \
-  _______, _______, _______, _______, KC_EQUAL,ALGR(KC_DOT),                     _______, _______, _______, _______, S(KC_PSCR), KC_DELETE, \
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_HOME, \
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                       KC_PGUP, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_END, \
-  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,   RALT(KC_7), RALT(KC_8),  KC_PGDN, KC_MINS, KC_EQL, KC_LCBR, KC_RCBR, KC_PIPE, \
-                             _______, _______, LM(_QWERTY, MOD_LSFT),  KC_LSFT, _______,  _______, _______, KC_RCTL \
+  _______,_______,_______,_______,KC_EQL ,GREATER,                _______,_______,_______,_______,SHFPSCR,KC_DEL ,\
+  KC_GRV ,KC_1   ,KC_2   ,KC_3   ,KC_4   ,KC_5   ,                KC_6   ,KC_7   ,KC_8   ,KC_9   ,KC_0   ,KC_HOME,\
+  KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,KC_F6  ,                KC_PGUP,KC_LEFT,KC_DOWN,KC_UP  ,KC_RGHT,KC_END ,\
+  KC_F7  ,KC_F8  ,KC_F9  ,KC_F10 ,KC_F11 ,KC_F12 ,BRKLEFT,BRKRGHT,KC_PGDN,KC_MINS,KC_EQL ,KC_LCBR,KC_RCBR,KC_PIPE,\
+                          _______,_______,LSQWERT,_______,_______,_______,KC_DEL ,KC_RCTL\
 ),
 [_RESET] = LAYOUT( \
-  _______,_______,_______,_______,_______, RESET ,                _______,_______,_______,_______,_______,_______, \
-  _______,_______,_______,_______,_______,_______,                _______,_______,_______,_______,_______,_______, \
-  _______,_______,_______,_______,_______,_______,                _______,_______,_______,_______,_______,_______, \
-  _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______, \
-                          _______,_______,_______,_______,_______,_______,_______,_______ \
+  _______,_______,_______,_______,_______, RESET ,                _______,_______,_______,_______,_______,_______,\
+  _______,_______,_______,_______,_______,_______,                _______,_______,_______,_______,_______,_______,\
+  _______,_______,_______,_______,_______,_______,                _______,_______,_______,_______,_______,_______,\
+  _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,\
+                          _______,_______,_______,_______,_______,_______,_______,_______\
 )
 };
 
@@ -146,7 +159,7 @@ void oled_task_user(void) {
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   if (record->event.pressed) {
 #ifdef OLED_DRIVER_ENABLE
-    set_keylog(keycode, record);
+    //set_keylog(keycode, record);
 #endif
     // set_timelog();
   }
