42a59c8073348d3dc70bc2b654b827c9e9dad373
Mathieu Fournier <contact@mathieufournier.ca>
2020-10-22 19:53:53 -0400

    Another improvement.

--- keyboards/lily58/keymaps/mathieuf20/keymap.c
+++ keyboards/lily58/keymaps/mathieuf20/keymap.c
@@ -15,6 +15,8 @@ enum layer_number {
   _QWERTY = 0,
   _LOWER,
   _RAISE,
+  _MACROS,
+  _SQWERT,
   _RESET
 };
 
@@ -23,11 +25,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |  /   |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  | BSPC |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  -   |
+ * | Tab  |   q  |   w  |   e  |   r  |   t  |                    |   y  |   u  |   i  |   o  |   p  |  -   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | ***1 |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  è   |
+ * | ***1 |   a  |   s  |   d  |   f  |   g  |-------.    ,-------|   h  |   j  |   k  |   l  |   ;  |  è   |
  * |------+------+------+------+------+------|   (   |    |    )  |------+------+------+------+------+------|
- * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   é  |RShift|
+ * |LShift|   z  |   x  |   c  |   v  |   b  |-------|    |-------|   n  |   m  |   ,  |   .  |   é  |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |BackSP| RALT |
  *                   |      |      |      |/       /         \      \ |      |      |      |
@@ -46,17 +48,42 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_LSFT,KC_Z   ,KC_X   ,KC_C   ,KC_V   ,KC_B   ,PARLEFT,PARRGHT,KC_N   ,KC_M   ,KC_COMM,KC_DOT ,KC_SLSH,KC_RSFT,\
                           KC_LALT,KC_LGUI,GOLOWER,KC_SPC ,KC_ENT ,GORAISE,KC_BSPC,KC_RALT\
 ),
+/* QWERTY
+ * ,-----------------------------------------.                    ,-----------------------------------------.
+ * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |      |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |      |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |      |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |      |  È   |
+ * |------+------+------+------+------+------|   <   |    |    >  |------+------+------+------+------+------|
+ * |      |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |      |      |   É  |      |
+ * `-----------------------------------------/       /     \      \-----------------------------------------'
+ *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |BackSP| RALT |
+ *                   |      |      |      |/       /         \      \ |      |      |      |
+ *                   `----------------------------'           '------''--------------------'
+ */
+#define GREATER ALGR(KC_DOT)
+#define SMALLER ALGR(KC_COMM)
+#define ACUTE_E LSFT(KC_SLSH)
+#define GRAVE_E LSFT(KC_QUOT)
+[_SQWERT] = LAYOUT( \
+  _______,_______,_______,_______,_______,_______,                _______,_______,_______,_______,_______,_______,\
+  _______,S(KC_Q),S(KC_W),S(KC_E),S(KC_R),S(KC_T),                S(KC_Y),S(KC_U),S(KC_I),S(KC_O),S(KC_P),_______,\
+  _______,S(KC_A),S(KC_S),S(KC_D),S(KC_F),S(KC_G),                S(KC_H),S(KC_J),S(KC_K),S(KC_L),_______,GRAVE_E,\
+  _______,S(KC_Z),S(KC_X),S(KC_C),S(KC_V),S(KC_B),SMALLER,GREATER,S(KC_N),S(KC_M),_______,_______,ACUTE_E,_______,\
+                          _______,_______,XXXXXXX,_______,_______,XXXXXXX,_______,_______\
+),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |RSTLYR|      |      |      |      |      |                    |      |      |      |      |      |DELETE|
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                    |  F7  |  F8  |  F9  | F10  | F11  | F12  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |   /  |   !  |   @  |   #  |   $  |   %  |-------.    ,-------|   ^  |   &  |   *  |   (  |   )  |   \  |
+ * |   ~  |   !  |   @  |   #  |   $  |   %  |-------.    ,-------|   ?  |   &  |   *  |   (  |   )  |   \  |
  * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
- * | PREV | PLAY | NEXT |VOLDWN|VOLUP | MUTE |-------|    |-------|   ~  |   _  |   +  |   ^  |   ç  |   à  |
+ * |      |      |      |      |      |      |-------|    |-------|   '  |   _  |   +  |   ^  |   ç  |   à  |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |DELETE| RGUI |
+ *                   |      |      |      | /       /       \SEnter\  |RAISE |DELETE| RGUI |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
@@ -65,38 +92,44 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #define GORESET MO(_RESET)
 #define TILDE   ALGR(KC_RBRC)
 #define RSQWERT LM(_QWERTY, MOD_RSFT)
+#define SHFTENT LSFT(KC_ENT)
+#define APSTRPH LSFT(KC_COMM)
+#define QUOTMRK LSFT(KC_DOT)
 [_LOWER] = LAYOUT( \
   GORESET,_______,_______,_______,_______,_______,                _______,_______,_______,_______,KC_PSCR,KC_DEL ,\
   KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,KC_F6  ,                KC_F7  ,KC_F8  ,KC_F9  ,KC_F10 ,KC_F11 ,KC_F12 ,\
-  KC_GRV ,KC_EXLM,KC_AT  ,KC_HASH,KC_DLR ,KC_PERC,                KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_TILD,\
-  KC_MPRV,KC_MPLY,KC_MNXT,KC_VOLD,KC_VOLU,KC_MUTE,SBRKLFT,SBRKRGT,TILDE  ,KC_UNDS,KC_PLUS,KC_LBRC,KC_RBRC,KC_BSLS,\
-                          _______,_______,_______,_______,_______,RSQWERT,KC_DEL ,KC_RGUI\
+  TILDE  ,KC_EXLM,KC_AT  ,KC_HASH,KC_DLR ,KC_PERC,                KC_CIRC,KC_AMPR,KC_ASTR,APSTRPH,QUOTMRK,KC_TILD,\
+  _______,_______,_______,_______,_______,_______,SBRKLFT,SBRKRGT,APSTRPH,KC_UNDS,KC_PLUS,KC_LBRC,KC_RBRC,KC_BSLS,\
+                          _______,_______,_______,_______,SHFTENT,_______,KC_DEL ,KC_RGUI\
 ),
 /* RAISE
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |      |      |      |      |   =  |   >  |                    |      |      |      |      |PRTSCR|DELETE|
+ * |      |      |      |      |   =  |   >  |                    | Mute | Prev | Play | Next |PRTSCR|Ctrl+V|
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |   `  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  | HOME |
+ * |      |      |      |      |      |      |                    | Vol+ | HOME | PgDn | PgUp | END  |Ctrl+C|
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |-------.    ,-------| PGUP | Left | Down |  Up  | Right|  END |
+ * |      |      |      |      |   <  |  >   |-------.    ,-------| Vol- | Left | Down |  Up  | Right|Ctrl+X|
  * |------+------+------+------+------+------|   {   |    |    }  |------+------+------+------+------+------|
- * |  F7  |  F8  |  F9  | F10  | F11  | F12  |-------|    |-------| PGDN |   -  |   =  |   ¨  |   Ç  |   À  |
+ * |      |      |      |      |      |      |-------|    |-------|   "  |   -  |   =  |   ¨  |   Ç  |   À  |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |DELETE| RCTL |
+ *                   | LAlt | LGUI |LOWER | /Space  /       \ Enter\  |RAISE |DELETE| RCTL |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
-#define GREATER ALGR(KC_DOT)
 #define LSQWERT LM(_QWERTY, MOD_LSFT)
 #define BRKLEFT RALT(KC_7)
 #define BRKRGHT RALT(KC_8)
 #define SHFPSCR S(KC_PSCR)
+#define CTRL_X  LCTL(KC_X)
+#define CTRL_C  LCTL(KC_C)
+#define CTRL_V  LCTL(KC_V)
+#define GOMACRO MO(_MACROS)
 [_RAISE] = LAYOUT( \
-  _______,_______,_______,_______,KC_EQL ,GREATER,                _______,_______,_______,_______,SHFPSCR,KC_DEL ,\
-  KC_GRV ,KC_1   ,KC_2   ,KC_3   ,KC_4   ,KC_5   ,                KC_6   ,KC_7   ,KC_8   ,KC_9   ,KC_0   ,KC_HOME,\
-  KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,KC_F6  ,                KC_PGUP,KC_LEFT,KC_DOWN,KC_UP  ,KC_RGHT,KC_END ,\
-  KC_F7  ,KC_F8  ,KC_F9  ,KC_F10 ,KC_F11 ,KC_F12 ,BRKLEFT,BRKRGHT,KC_PGDN,KC_MINS,KC_EQL ,KC_LCBR,KC_RCBR,KC_PIPE,\
-                          _______,_______,LSQWERT,_______,_______,_______,KC_DEL ,KC_RCTL\
+  _______,_______,_______,_______,KC_EQL ,GREATER,                KC_MUTE,KC_MPRV,KC_MPLY,KC_MNXT,SHFPSCR,CTRL_V ,\
+  _______,_______,_______,_______,_______,_______,                KC_VOLU,KC_HOME,KC_PGDN,KC_PGUP,KC_END ,CTRL_C ,\
+  _______,_______,_______,_______,SMALLER,GREATER,                KC_VOLD,KC_LEFT,KC_DOWN,KC_UP  ,KC_RGHT,CTRL_X ,\
+  _______,_______,_______,_______,_______,_______,BRKLEFT,BRKRGHT,QUOTMRK,KC_MINS,KC_EQL ,KC_LCBR,KC_RCBR,KC_PIPE,\
+                          GOMACRO,_______,_______,_______,_______,_______,KC_DEL ,KC_RCTL\
 ),
 [_RESET] = LAYOUT( \
   _______,_______,_______,_______,_______, RESET ,                _______,_______,_______,_______,_______,_______,\
@@ -106,15 +139,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                           _______,_______,_______,_______,_______,_______,_______,_______\
 )
 };
-
-// Setting ADJUST layer RGB back to default
-void update_tri_layer_RGB(uint8_t layer1, uint8_t layer2, uint8_t layer3) {
-  if (IS_LAYER_ON(layer1) && IS_LAYER_ON(layer2)) {
-    layer_on(layer3);
-  } else {
-    layer_off(layer3);
-  }
-}
 
 //SSD1306 OLED update loop, make sure to enable OLED_DRIVER_ENABLE=yes in rules.mk
 #ifdef OLED_DRIVER_ENABLE
@@ -163,5 +187,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 #endif
     // set_timelog();
   }
+
   return true;
+}
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+  state = update_tri_layer_state(state, _LOWER, _RAISE, _SQWERT);
+  return state;
 }
