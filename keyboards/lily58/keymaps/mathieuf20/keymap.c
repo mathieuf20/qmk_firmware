edb852f9b215d91dae7768f913c1b261ec0d5a6c
Mathieu Fournier <contact@mathieufournier.ca>
2020-11-25 15:06:37 -0500

    Sporadic iteration

--- keyboards/lily58/keymaps/mathieuf20/keymap.c
+++ keyboards/lily58/keymaps/mathieuf20/keymap.c
@@ -18,7 +18,8 @@ enum layer_number {
   _VIRT1,
   _DIGITS,
   _SQWERT,
-  _RESET
+  _LYRCTL,
+  _GAME
 };
 
 // Qwerty
@@ -33,16 +34,21 @@ enum layer_number {
 #define SMALLER ALGR(KC_COMM)
 #define ACUTE_E LSFT(KC_SLSH)
 #define GRAVE_E LSFT(KC_QUOT)
+#define COLON   LSFT(KC_SCLN)
 
 // Lower
 #define SBRKLFT RALT(KC_9)
 #define SBRKRGT RALT(KC_0)
-#define GORESET MO(_RESET)
+#define PIPE    RALT(KC_GRV)
+#define GOLCTL  LT(_LYRCTL, KC_TILD)
 #define TILDE   ALGR(KC_RBRC)
 #define RSQWERT LM(_QWERTY, MOD_RSFT)
 #define SHFTENT LSFT(KC_ENT)
 #define APSTRPH LSFT(KC_COMM)
 #define QUOTMRK LSFT(KC_DOT)
+#define FRGUILL RALT(KC_Z)
+#define FRGUILR RALT(KC_X)
+#define BACKTCK RALT(KC_LBRC)
 
 // Raise
 #define LSQWERT LM(_QWERTY, MOD_LSFT)
@@ -53,8 +59,21 @@ enum layer_number {
 #define CTRL_C  LCTL(KC_C)
 #define CTRL_V  LCTL(KC_V)
 #define GODIGIT MO(_DIGITS)
+#define GOVIRT1 MO(_VIRT1)
+
+// Layers
+#define DF_GAME DF(_GAME)
+#define DF_QWER DF(_QWERTY)
+#define BSPLCTL LT(_LYRCTL, KC_BSPC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+[_GAME] = LAYOUT( \
+  DF_QWER,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,                KC_GRV ,KC_1   ,KC_2   ,KC_3   ,KC_4   ,KC_5   ,\
+  XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,                KC_TAB ,KC_Q   ,KC_W   ,KC_E   ,KC_R   ,KC_T   ,\
+  XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,                KC_ESC ,KC_A   ,KC_S   ,KC_D   ,KC_F   ,KC_G   ,\
+  XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,KC_LSFT,KC_LSFT,KC_Z   ,KC_X   ,KC_C   ,KC_V   ,KC_SPC ,\
+                          XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,KC_LCTL,KC_SPC ,KC_BSPC,DF_QWER\
+),
 /* QWERTY
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |  /   |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  | BSPC |
@@ -71,74 +90,74 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ***1: LCTL when held, ESC when tapped
  */
  [_QWERTY] = LAYOUT(
-  KC_GRV ,KC_1   ,KC_2   ,KC_3   ,KC_4   ,KC_5   ,                KC_6   ,KC_7   ,KC_8   ,KC_9   ,KC_0   ,KC_BSPC,\
+  KC_GRV ,KC_1   ,KC_2   ,KC_3   ,KC_4   ,KC_5   ,                KC_6   ,KC_7   ,KC_8   ,KC_9   ,KC_0   ,BSPLCTL,\
   KC_TAB ,KC_Q   ,KC_W   ,KC_E   ,KC_R   ,KC_T   ,                KC_Y   ,KC_U   ,KC_I   ,KC_O   ,KC_P   ,KC_MINS,\
   CTRLESC,KC_A   ,KC_S   ,KC_D   ,KC_F   ,KC_G   ,                KC_H   ,KC_J   ,KC_K   ,KC_L   ,KC_SCLN,KC_QUOT,\
   KC_LSFT,KC_Z   ,KC_X   ,KC_C   ,KC_V   ,KC_B   ,PARLEFT,PARRGHT,KC_N   ,KC_M   ,KC_COMM,KC_DOT ,KC_SLSH,KC_RSFT,\
                           KC_LALT,KC_LGUI,GOLOWER,KC_SPC ,KC_ENT ,GORAISE,KC_BSPC,KC_RALT\
 ),
-/* QWERTY
+/* SQWERTY
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
+ * |   |  |   !  |   @  |   #  |   $  |   %  |                    |   ?  |   &  |   *  |   (  |   )  |   ~  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |      |
+ * |   ~  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |      |  È   |
+ * |      |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   :  |  È   |
  * |------+------+------+------+------+------|   <   |    |    >  |------+------+------+------+------+------|
- * |      |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |      |      |   É  |      |
+ * |Space |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   '  |   "  |   É  |Space |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |BackSP| RALT |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
 [_SQWERT] = LAYOUT( \
-  _______,S(KC_1),S(KC_2),S(KC_3),S(KC_4),S(KC_5),                S(KC_6),S(KC_7),S(KC_8),S(KC_9),S(KC_0),_______,\
-  _______,S(KC_Q),S(KC_W),S(KC_E),S(KC_R),S(KC_T),                S(KC_Y),S(KC_U),S(KC_I),S(KC_O),S(KC_P),_______,\
-  _______,S(KC_A),S(KC_S),S(KC_D),S(KC_F),S(KC_G),                S(KC_H),S(KC_J),S(KC_K),S(KC_L),_______,GRAVE_E,\
-  _______,S(KC_Z),S(KC_X),S(KC_C),S(KC_V),S(KC_B),SMALLER,GREATER,S(KC_N),S(KC_M),_______,_______,ACUTE_E,_______,\
-                          _______,_______,XXXXXXX,_______,_______,XXXXXXX,_______,_______\
+  PIPE   ,S(KC_1),S(KC_2),S(KC_3),S(KC_4),S(KC_5),                S(KC_6),S(KC_7),S(KC_8),S(KC_9),S(KC_0),TILDE  ,\
+  TILDE  ,S(KC_Q),S(KC_W),S(KC_E),S(KC_R),S(KC_T),                S(KC_Y),S(KC_U),S(KC_I),S(KC_O),S(KC_P),_______,\
+  _______,S(KC_A),S(KC_S),S(KC_D),S(KC_F),S(KC_G),                S(KC_H),S(KC_J),S(KC_K),S(KC_L),COLON  ,GRAVE_E,\
+  KC_SPC ,S(KC_Z),S(KC_X),S(KC_C),S(KC_V),S(KC_B),SMALLER,GREATER,S(KC_N),S(KC_M),APSTRPH,QUOTMRK,ACUTE_E,KC_SPC ,\
+                          _______,_______,XXXXXXX,KC_SPC ,_______,XXXXXXX,KC_BSPC,_______\
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |RSTLYR|      |      |      |      |      |                    |      |      |      |      |      |DELETE|
+ * |GOLCTL|      |      |      |      |      |                    |      |      |      |      |      |DELETE|
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                    |  F7  |  F8  |  F9  | F10  | F11  | F12  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |   ~  |   !  |   @  |   #  |   $  |   %  |-------.    ,-------|   ?  |   &  |   *  |   (  |   )  |   \  |
+ * |   /  |   !  |   @  |   #  |   $  |   %  |-------.    ,-------|   ?  |   &  |   *  |   '  |   "  |   \  |
  * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
- * |      |      |      |      |      |      |-------|    |-------|   '  |   _  |   +  |   ^  |   ç  |   à  |
+ * |   ~  |   «  |   »  |      |      |      |-------|    |-------|   `  |   _  |   +  |   ^  |   ç  |   à  |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   |      |      |      | /       /       \SEnter\  |RAISE |DELETE| RGUI |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
 [_LOWER] = LAYOUT( \
-  GORESET,_______,_______,_______,_______,_______,                _______,_______,_______,_______,KC_PSCR,KC_DEL ,\
+  GOLCTL ,_______,_______,_______,_______,_______,                _______,_______,_______,_______,KC_PSCR,KC_DEL ,\
   KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,KC_F6  ,                KC_F7  ,KC_F8  ,KC_F9  ,KC_F10 ,KC_F11 ,KC_F12 ,\
-  TILDE  ,KC_EXLM,KC_AT  ,KC_HASH,KC_DLR ,KC_PERC,                KC_CIRC,KC_AMPR,KC_ASTR,APSTRPH,QUOTMRK,KC_TILD,\
-  _______,_______,_______,_______,_______,_______,SBRKLFT,SBRKRGT,APSTRPH,KC_UNDS,KC_PLUS,KC_LBRC,KC_RBRC,KC_BSLS,\
+  KC_GRV ,KC_EXLM,KC_AT  ,KC_HASH,KC_DLR ,KC_PERC,                KC_CIRC,KC_AMPR,KC_ASTR,APSTRPH,QUOTMRK,KC_TILD,\
+  TILDE  ,FRGUILL,FRGUILR,_______,_______,_______,SBRKLFT,SBRKRGT,BACKTCK,KC_UNDS,KC_PLUS,KC_LBRC,KC_RBRC,KC_BSLS,\
                           _______,_______,_______,_______,SHFTENT,_______,KC_DEL ,KC_RGUI\
 ),
 /* RAISE
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |      |      |      |      |   =  |   >  |                    | Mute | Prev | Play | Next |PRTSCR|Ctrl+V|
+ * |GOLCTL|   ~  |      |      |   =  |   >  |                    | Mute | Prev | Play | Next |PRTSCR|Ctrl+V|
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |      |      |      |      |      |      |                    | Vol+ | HOME | PgDn | PgUp | END  |Ctrl+C|
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |      |      |      |      |      |      |-------.    ,-------| Vol- | Left | Down |  Up  | Right|Ctrl+X|
  * |------+------+------+------+------+------|   {   |    |    }  |------+------+------+------+------+------|
- * |      |      |      |      |      |      |-------|    |-------|   "  |   -  |   =  |   ¨  |   Ç  |   À  |
+ * |      |      |      |      |      |      |-------|    |-------|   ~  |   -  |   =  |   ¨  |   Ç  |   À  |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   | LAlt | LGUI |LOWER | /DIGITS /       \ Enter\  |RAISE |DELETE| RCTL |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
 [_RAISE] = LAYOUT( \
-  _______,_______,_______,_______,KC_EQL ,GREATER,                KC_MUTE,KC_MPRV,KC_MPLY,KC_MNXT,SHFPSCR,CTRL_V ,\
+  KC_TILD,TILDE  ,_______,_______,KC_EQL ,GREATER,                KC_MUTE,KC_MPRV,KC_MPLY,KC_MNXT,SHFPSCR,CTRL_V ,\
   _______,_______,_______,_______,_______,_______,                KC_VOLU,KC_HOME,KC_PGDN,KC_PGUP,KC_END ,CTRL_C ,\
   _______,_______,_______,_______,_______,_______,                KC_VOLD,KC_LEFT,KC_DOWN,KC_UP  ,KC_RGHT,CTRL_X ,\
-  _______,_______,_______,_______,_______,_______,BRKLEFT,BRKRGHT,QUOTMRK,KC_MINS,KC_EQL ,KC_LCBR,KC_RCBR,KC_PIPE,\
-                          _______,MO(_VIRT1),_______,GODIGIT,_______,_______,KC_DEL ,KC_RCTL\
+  _______,_______,_______,_______,_______,_______,BRKLEFT,BRKRGHT,TILDE  ,KC_MINS,KC_EQL ,KC_LCBR,KC_RCBR,KC_PIPE,\
+                          _______,GOVIRT1,_______,GOVIRT1,_______,_______,KC_DEL ,KC_RCTL\
 ),
 /* DIGITS
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -147,7 +166,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |      |   1  |   2  |   3  |   4  |   5  |-------.    ,-------|   6  |   7  |   8  |   9  |   0  |      |
- * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
+ * |------+------+------+------+------+------|   «   |    |   »   |------+------+------+------+------+------|
  * |      |      |      |      |      |      |-------|    |-------|      |      |      |      |      |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   | LAlt | LGUI |LOWER | /Space  /       \Enter \  |RAISE |BackSP| RALT |
@@ -158,17 +177,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______,_______,_______,_______,_______,_______,                _______,_______,_______,_______,_______,_______,\
   _______,_______,_______,_______,_______,_______,                _______,_______,_______,_______,_______,_______,\
   _______,KC_1   ,KC_2   ,KC_3   ,KC_4   ,KC_5   ,                KC_6   ,KC_7   ,KC_8   ,KC_9   ,KC_0   ,_______,\
-  _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,\
+  _______,_______,_______,_______,_______,_______,FRGUILL,FRGUILR,_______,_______,_______,_______,_______,_______,\
                           _______,_______,_______,_______,_______,_______,_______,_______\
 ),
-[_RESET] = LAYOUT( \
+[_LYRCTL] = LAYOUT( \
   _______,_______,_______,_______,_______, RESET ,                _______,_______,_______,_______,_______,_______,\
   _______,_______,_______,_______,_______,_______,                _______,_______,_______,_______,_______,_______,\
   _______,_______,_______,_______,_______,_______,                _______,_______,_______,_______,_______,_______,\
   _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,\
-                          _______,_______,_______,_______,_______,_______,_______,_______\
+                          _______,_______,_______,_______,_______,_______,_______,DF_GAME\
 )
 };
+
 
 //SSD1306 OLED update loop, make sure to enable OLED_DRIVER_ENABLE=yes in rules.mk
 #ifdef OLED_DRIVER_ENABLE
@@ -187,6 +207,7 @@ const char *read_keylog(void);
 const char *read_keylogs(void);
 
 void print_hydra_logo(void);
+void print_sherweb_logo(void);
 // const char *read_mode_icon(bool swap);
 // const char *read_host_led_state(void);
 // void set_timelog(void);
@@ -205,7 +226,7 @@ void oled_task_user(void) {
     print_hydra_logo();
   } else {
     //oled_write(read_logo(), false);
-    print_hydra_logo();
+    print_sherweb_logo();
   }
 }
 #endif // OLED_DRIVER_ENABLE
